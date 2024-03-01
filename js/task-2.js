b2efdcc4d32b9bff307b5020cac5d09df7be018d
Oleh Osaulenko <oleh.osaulenko@vista.com>
2024-03-01 16:50:25 +0200

    Init commit

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function makeArray(firstArray, secondArray, maxLength) {
+    let array = firstArray.concat(secondArray);
+
+    if(array.length > maxLength) {
+        return array.slice(0, maxLength);
+    }
+
+    return array;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
